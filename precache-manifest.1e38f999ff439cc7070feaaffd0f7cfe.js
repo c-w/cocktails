ace6b6d1841cf6abdd27eb37313ce3611bb68fec
Clemens Wolff <clewolff@microsoft.com>
2018-11-18 19:17:47 -0500

    Updates

---
+++ precache-manifest.1e38f999ff439cc7070feaaffd0f7cfe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "27c88389f00e0d8bfeebebae81f240b6",
+    "url": "/cocktails-blog/static/media/icons.27c88389.woff2"
+  },
+  {
+    "revision": "d0bfd066162b37ab53fb",
+    "url": "/cocktails-blog/static/css/main.ff8bc652.chunk.css"
+  },
+  {
+    "revision": "b42b446772f84a277ee29a0615b38800",
+    "url": "/cocktails-blog/static/media/icons.b42b4467.svg"
+  },
+  {
+    "revision": "cd6f1569ea8a0b4e6264",
+    "url": "/cocktails-blog/static/js/1.cd6f1569.chunk.js"
+  },
+  {
+    "revision": "a80be6d8d997de6e3f22",
+    "url": "/cocktails-blog/static/js/runtime~main.a80be6d8.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/cocktails-blog/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "d0bfd066162b37ab53fb",
+    "url": "/cocktails-blog/static/js/main.d0bfd066.chunk.js"
+  },
+  {
+    "revision": "7bc63d50d4158c6bfd4b30a99c9f9460",
+    "url": "/cocktails-blog/static/media/icons.7bc63d50.woff"
+  },
+  {
+    "revision": "d3490a32350db6c9c41e69cc9ce99cd0",
+    "url": "/cocktails-blog/static/media/icons.d3490a32.ttf"
+  },
+  {
+    "revision": "263bfe56755cefdc9e6c3d8070e0868d",
+    "url": "/cocktails-blog/static/media/icons.263bfe56.eot"
+  },
+  {
+    "revision": "cd6f1569ea8a0b4e6264",
+    "url": "/cocktails-blog/static/css/1.f7e5b8f8.chunk.css"
+  },
+  {
+    "revision": "2ef910db665d95a3ac619bce93e697d3",
+    "url": "/cocktails-blog/index.html"
+  }
+];
